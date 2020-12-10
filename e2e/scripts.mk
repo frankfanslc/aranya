0a63e76ec145122536ee9413b2a73c2215d87bd6
donorp <donorp@arhat.solutions>
2020-12-11 00:00:58 +0100

    e2e: Add kubernetes v1.20.x tests

--- e2e/scripts.mk
+++ e2e/scripts.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+e2e.v1-20:
+	sh e2e/suite.sh v1-20
+
 e2e.v1-19:
 	sh e2e/suite.sh v1-19
 
